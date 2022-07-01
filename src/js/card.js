5a185aa79feeee2e75fc073351f7c04fb25ad65c
Farid <faridkarmanov@gmail.com>
2022-07-01 12:10:56 +0300

    feat(button): add click on all button

--- src/js/card.js
+++ src/js/card.js
@@ -6,17 +6,12 @@ function renderCard(cards) {
     const cardElement = document.createElement("div");
     cardElement.classList.add("product__card");
     cardElement.innerHTML = `
-  <div class="product__card__image">
     <img src="${card.image}" alt="photo">
-  </div>
-  <div class="product__card__info">
     <h3 class="product__card__title">Наименование: ${card.name}</h3>
     <p class="product__card__previous">Цена со скидкой: ${card.previousPrice}</p>
     <p class="product__card__price">Цена: ${card.price}</p>
-  </div>
-  <div class="product__info-plus">
-      <button class="button"><i class="fa-solid fa-square-plus add-cart" id="${card.id}"></i></button>
-    </div>
+    <button class="button fa-solid fa-square-plus add-cart" data-id="${card.id}"></button>
+    
   `;
 
     productCards.append(cardElement);
@@ -24,51 +19,7 @@ function renderCard(cards) {
 }
 
 let productCards = document.querySelector(".product__cards");
-
-function renderCardProduct(card) {
-  const cartList = document.querySelector(".header__basket-list"); // ul
-  const cartElement = document.createElement("li"); // li - элемент
-  cartElement.innerHTML = `
-  <article class="header__basket-product cart-product">
-  <img
-    src="${card.image}"
-    alt="MacBook"
-    class="cart-product__photo"
-  />
-  <div class="cart-product__text">
-    <h3 class="cart-product__description">
-      ${card.name}
-    </h3>
-    <span class="cart-product__price">${card.price}</span>
-  </div>
-  <button class="cart-product__delete">X</button>
-</article>
-`;
-
-  cartList.append(cartElement);
-}
-
-const cartList = document.querySelector(".header__basket-list");
-const clearCart = document.querySelector(".header__basket-button");
-
-clearCart.addEventListener("click", () => (cartList = ""));
-
-productCards.addEventListener("click", (event) => {
-  if (event.target.id) {
-    const ID = event.target.id;
-    fetchOneCard(ID).then((json) => renderCardProduct(json));
-  }
-});
-
-function fetchOneCard(id) {
-  return fetch(`https://62b5dfa342c6473c4b3c12c2.mockapi.io/card/${id}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
-      return json;
-    });
-}
+let products = null;
 
 function fetchCards() {
   return fetch("https://62b5dfa342c6473c4b3c12c2.mockapi.io/card")
@@ -81,5 +32,6 @@ function fetchCards() {
 }
 
 fetchCards().then((json) => {
+  products = json;
   renderCard(json);
 });
--- src/js/cart.js
+++ src/js/cart.js
@@ -1,32 +1,65 @@
-const cart = document.querySelector(".header__basket-logo");
+const cart = document.querySelector(".header__basket-logo"); // button cart
+const cartList = document.querySelector(".header__basket-list"); // ul
+const cartDelete = document.querySelector(".cart-product__delete"); // button delete card from cart
+const cartProducts = [];
+console.log(cartProducts);
 
-cart.addEventListener("click", () => {
-  if (cartList.style.display == "none") {
-    cartList.style.display = "block";
-  } else if (cartList.style.display !== "none") {
-    cartList.style.display = "none";
+cartList.addEventListener("click", (event) => {
+  if (event.target.dataset.id) {
+    const ID = event.target.dataset.id;
+    for (let cartProduct of cartProducts) {
+      if (cartProduct.id == ID) {
+        let li = document.getElementById(`${ID}`);
+        li.remove();
+        cartProducts.forEach(function (el, i) {
+          if (el.id == ID) cartProducts.splice(i, 1);
+        });
+      }
+    }
   }
 });
 
-function renderCardProduct(card) {
-  // ul
-  const cartElement = document.createElement("li"); // li - элемент
+cart.addEventListener("click", () => (cartList.style.display = "block"));
+
+function renderCartProduct(card) {
+  const cartElement = document.createElement("li"); // li
+  cartElement.setAttribute("id", `${card.id}`);
   cartElement.innerHTML = `
-  <article class="header__basket-product cart-product">
-  <img
-    src="${card.image}"
-    alt="MacBook"
-    class="cart-product__photo"
-  />
-  <div class="cart-product__text">
+    <article class="header__basket-product cart-product">
+    <img
+      src="${card.image}"
+      alt="MacBook"
+      class="cart-product__photo"
+    />
+    <div class="cart-product__text">
     <h3 class="cart-product__description">
-      ${card.name}
+    ${card.name}
     </h3>
     <span class="cart-product__price">${card.price}</span>
-  </div>
-  <button class="cart-product__delete">X</button>
-</article>
-`;
-
+    </div>
+    <button class="cart-product__delete" data-id=${card.id}>X</button>
+    </article>
+    `;
+  cartProducts.push(cartElement);
   cartList.append(cartElement);
 }
+
+const clearCart = document.querySelector(".header__basket-button"); // delete all cart list
+clearCart.addEventListener("click", () => {
+  (cartList.innerHTML = "") & (cartList.style.display = "none");
+  cartProducts.length = 0;
+});
+
+productCards.addEventListener("click", (event) => {
+  if (event.target.dataset.id) {
+    const ID = event.target.dataset.id;
+    if (products == null) {
+      console.log("ничего нет");
+    } else
+      for (let card of products) {
+        if (ID == card.id) {
+          renderCartProduct(card);
+        }
+      }
+  }
+}); // закидывание в корзину карточки
